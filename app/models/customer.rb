11f215847f20e1242d2a430e9c1aebe6c8b74e55
zandersantos1 <zainzedd@gmail.com>
2025-03-27 18:48:15 -0500

    Added ransackable attributes to Customers

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,7 @@
 class Customer < ApplicationRecord
   has_one_attached :image
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["EmailAddress", "FullName", "Notes", "PhoneNumber", "created_at", "id", "updated_at"]
+  end
 end
