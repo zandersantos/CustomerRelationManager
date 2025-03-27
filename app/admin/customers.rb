dfbbf6767398e55477305ee48ee22935152a6afe
zandersantos1 <zainzedd@gmail.com>
2025-03-27 18:32:03 -0500

    Installed sass-rails, created a config folder under app/admin, and created and updated a manifest.js folder underneath the newly created config folder

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -1,31 +1,19 @@
 ActiveAdmin.register Customer do
+  permit_params :full_name, :phone_number, :email, :notes, :image
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :FullName, :PhoneNumber, :EmailAddress, :Notes
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:FullName, :PhoneNumber, :EmailAddress, :Notes]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
+  remove_filter :image_attachment, :image_blob
 
-  ActiveAdmin.register Customer do
-    permit_params :FullName, :PhoneNumber, :EmailAddress, :Notes
-
-    remove_filter :image_attachment, :image_blob
-
-    form do |f|
-      f.semantic_errors
-      f.inputs
-      f.inputs do
-        f.input :image, as :file
-      end
-      f.actions
+  form do |f|
+    f.inputs do
+      f.input :full_name
+      f.input :phone_number
+      f.input :email
+      f.input :notes
     end
+    f.semantic_errors
+    f.inputs do
+      f.input :image, as: :file
+    end
+    f.actions
   end
+end
