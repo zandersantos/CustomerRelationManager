a039a436796fb611419d8193586c8bc04f1311e3
zandersantos1 <zainzedd@gmail.com>
2025-03-27 18:50:56 -0500

    Added validations for the FullName and EmailAddress in the Customer Model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,9 @@
 class Customer < ApplicationRecord
   has_one_attached :image
 
+  validates :FullName, presence: true
+
+  validates :EmailAddress, presence: true, uniqueness: true
   def self.ransackable_attributes(auth_object = nil)
     ["EmailAddress", "FullName", "Notes", "PhoneNumber", "created_at", "id", "updated_at"]
   end
