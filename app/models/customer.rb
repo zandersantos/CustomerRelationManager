8caa6f41b934804fc3718f454a72095b94209298
zandersantos1 <zainzedd@gmail.com>
2025-03-27 18:51:35 -0500

    Updated the EmailAddress validations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,7 +3,7 @@ class Customer < ApplicationRecord
 
   validates :FullName, presence: true
 
-  validates :EmailAddress, presence: true, uniqueness: true
+  validates :EmailAddress, uniqueness: true
   def self.ransackable_attributes(auth_object = nil)
     ["EmailAddress", "FullName", "Notes", "PhoneNumber", "created_at", "id", "updated_at"]
   end
