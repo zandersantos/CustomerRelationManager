aeb9ccaed96a0b4a19291e8673da8be393b685eb
zandersantos1 <zainzedd@gmail.com>
2025-03-27 18:58:39 -0500

    Fixed Customer Email Address Validations to allow for blanks

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,7 +3,7 @@ class Customer < ApplicationRecord
 
   validates :FullName, presence: true
 
-  validates :EmailAddress, uniqueness: true
+  validates :EmailAddress, uniqueness: true, allow_blank: true
   def self.ransackable_attributes(auth_object = nil)
     ["EmailAddress", "FullName", "Notes", "PhoneNumber", "created_at", "id", "updated_at"]
   end
