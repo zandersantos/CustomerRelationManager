78b2dd1d9a104fd4e3b3090c83cafbd66641bcde
zandersantos1 <zainzedd@gmail.com>
2025-03-27 18:53:13 -0500

    Updated the Customer Admin parameters to be correctly named

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -1,14 +1,14 @@
 ActiveAdmin.register Customer do
-  permit_params :full_name, :phone_number, :email, :notes, :image
+  permit_params :FullName, :PhoneNumber, :EmailAddress, :Notes, :image
 
   remove_filter :image_attachment, :image_blob
 
   form do |f|
     f.inputs do
-      f.input :full_name
-      f.input :phone_number
-      f.input :email
-      f.input :notes
+      f.input :FullName
+      f.input :PhoneNumber
+      f.input :EmailAddress
+      f.input :Notes
     end
     f.semantic_errors
     f.inputs do
